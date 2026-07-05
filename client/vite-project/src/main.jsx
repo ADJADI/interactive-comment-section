1ac28699dbbe0001f5b2e9eddc7bf891d6b02877
ADJADI <mradjadi.a@gmail.com>
2026-07-05 18:06:59 +0200

    restore import

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -1,16 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./styles/index.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   // En dev : proxy Vite vers localhost:4000. En prod : chemin relatif /graphql
   // derrière le reverse proxy nginx (voir nginx.conf), même domaine que le front.
-  uri: import.meta.env.VITE_GRAPHQL_URI || "http
+  uri: import.meta.env.VITE_GRAPHQL_URI || "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
